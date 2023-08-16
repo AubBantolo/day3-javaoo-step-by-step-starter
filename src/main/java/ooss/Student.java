4dc3258823aee1616e52805be742fac0c6cd3552
AubBantolo <jhuliet14@gmail.com>
2023-08-16 21:31:40 +0800

    feat: completed all tests from steps 2-4 added

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,5 +1,49 @@
 package ooss;
 
-public class Student extends Person {
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Optional;
 
+public class Student extends Person implements EventListener {
+    private Klass klass;
+
+    private String name;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String classIn = Optional.ofNullable(this.klass)
+                .filter(studentKlass -> isIn(studentKlass))
+                .map(studentKlass -> String.format(" I am in class %d.", studentKlass.getId()))
+                .orElse("");
+
+        return String.format("%s I am a student.%s", super.introduce(), classIn);
+    }
+
+    public void join(Klass klass) {
+        this.klass = klass;
+        klass.subscribe(this);
+    }
+
+    public boolean isIn(Klass klass) {
+        return Optional.ofNullable(this.klass)
+                .filter(studentKlass -> studentKlass.equals(klass))
+                .isPresent();
+    }
+
+    @Override
+    public void updateLeader(Klass klass, Student student) {
+        System.out.printf("I am %s, student of Class %d. I know %s become Leader."
+                , this.getName()
+                , klass.getId()
+                , student.getName()
+        );
+    }
 }
--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,9 +1,48 @@
 package ooss;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Teacher extends Person {
+public class Teacher extends Person implements EventListener {
+    private List<Klass> klassList = new ArrayList<>();
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce(){
+        String classIn = klassList.stream()
+                .filter(klass -> belongsTo(klass))
+                .map(klass -> String.valueOf(klass.getId()))
+                .reduce((klassResultId, klassId) -> klassResultId + ", " + klassId)
+                .map(klassResult -> String.format(" I teach Class %s.", klassResult))
+                .orElse("");
+
+        return String.format("%s I am a teacher.%s", super.introduce(), classIn);
+
+    }
+
+    @Override
+    public void updateLeader(Klass klass, Student student) {
+        System.out.printf("I am %s, teacher of Class %d. I know %s become Leader."
+                , this.getName()
+                , klass.getId()
+                , student.getName()
+        );
+    }
+
+    public void assignTo(Klass klass) {
+        this.klassList.add(klass);
+        klassList.stream().forEach(teacherKlass -> teacherKlass.subscribe(this));
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return this.klassList.contains(klass);
+        
+    }
+
+    public boolean isTeaching(Student tom) {
+        return this.klassList.contains(tom.getKlass());
+    }
 }
